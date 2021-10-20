c4d008281ffe7a18fb74b8d240498a5f65f94564
beefykenny <hoftkenny@gmail.com>
2021-10-20 07:54:19 -0300

    set prod URL to https

--- src/utils/apiUrl.js
+++ src/utils/apiUrl.js
@@ -1,4 +1,4 @@
 const URLDev = "http://localhost:8000/api/";
-const URLProd = "http://symor-richaards.herokuapp.com/api/";
+const URLProd = "https://symor-richaards.herokuapp.com/api/";
 
 export default URLProd;
